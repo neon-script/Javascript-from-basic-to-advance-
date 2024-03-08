aadb50af9519b9c4e9065b705b9f1b8be33464eb
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2024-03-08 20:42:49 +0530

    index2.js

---
+++ functions/index2.js
@@ -0,0 +1,11 @@
+function add(num1,num2)// parameters
+{
+  console.log(num1+num2)
+}
+add(2,5)// arguments
+function multiply(num1,num2)
+{
+   return num1*num2
+}
+ const m= multiply(6,7)
+ console.log(m+4);

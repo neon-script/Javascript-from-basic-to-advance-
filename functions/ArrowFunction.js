3c01de606e338f117d7eafecc3ca9036d44218bd
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2024-03-10 00:06:18 +0530

    Create ArrowFunction.js

---
+++ functions/ArrowFunction.js
@@ -0,0 +1,29 @@
+const Hello=()=>
+  {
+    console.log('Hello');
+  }
+Hello();
+
+// add two numbers using arrow function
+const addSum=(a,b)=>
+  {
+    return a+b;
+  }
+console.log(addSum(3,2));
+
+const diff=(a,b)=>a-b;
+console.log(diff(6,2));//4
+
+
+const obj=
+  {
+    name:'radhika',
+    age:18,
+    myFunction:function()
+      {
+        console.log(thos.value);
+  },
+  };
+obj.function()
+
+
